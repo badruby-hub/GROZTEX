a7d7190cd89fc7e5f506639455486a090a0913a3
Nazim Fataliev <nazim.rabota@bk.ru>
2025-07-15 00:07:48 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-import HomePage from"./pages/Home.js";
-import NoFoundPage from"./pages/NotFoundpage.js";
-import AboutPage from"./pages/About.js";
-import ExchangePage from"./pages/Exchange.js";
-import RequestsPage from"./pages/Requests.js";
-import SupportPage from"./pages/Support.js";
-import ValuationPage from"./pages/Valuation.js";
+import HomePage from "./pages/Home";
+import NoFoundPage from "./pages/NotFoundpage";
+import AboutPage from "./pages/About";
+import ExchangePage from "./pages/Exchange";
+import RequestsPage from "./pages/Requests";
+import SupportPage from "./pages/Support";
+import ValuationPage from "./pages/Valuation";
 
 
 
