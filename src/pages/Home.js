b6fcc98b32968348166af0f98ea4af5c93a0ec23
Nazim Fataliev <nazim.rabota@bk.ru>
2025-07-16 00:02:19 +0300

    adding logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,14 +18,14 @@ export default function HomePage() {
        
         <div className="block__content__home">
            <div className="App">
-             <div className='logo'>GROZ⍑EX</div>
+             <div className='logo'><div className="logo__name">GROZ<img className="svg__logo" src="./logo.svg"/>EX</div></div>
              <ul>
               <li className='exchange'><Link className='link' to='/exchange'>Купить/Продать USDT</Link></li>
               <li className='about'><Link className='link' to='/about'>О нас</Link></li>
               <li className='requests'><Link className='link' to='/requests'>Все заявки</Link></li>
               <li className='support'><Link className='link' to='/support'>Поддержка</Link></li>
               <li className='valuation'><Link className='link' to='/valuation'>Курс</Link></li>
-              <li onClick={onClose} className='close'><div className='btn__close'>закрыть приложение</div></li>
+              <li onClick={onClose} className='close'><div className='btn__close'>Закрыть приложение</div></li>
              </ul>
            </div>
            
