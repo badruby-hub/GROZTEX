026ee37b47f5f1e8b4c850cfd348a3ff0a5ff3ee
Nazim Fataliev <nazim.rabota@bk.ru>
2025-07-26 17:47:25 +0300

    поправка ошибок

--- components/Navigation/Header.jsx
+++ components/Navigation/Header.jsx
@@ -12,9 +12,9 @@ export default function Header() {
      if (typeof window !== 'undefined') {
        const telegram = window.Telegram?.WebApp;
        if (telegram) {
-         telegram.ready();
-         telegram.expand();
          setTg(telegram);
+          tg.ready();
+         tg.expand();
          };
        }
    }, []);
