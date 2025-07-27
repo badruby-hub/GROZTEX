d9ecc5a4261ecf952f46bce7566de629dfacab6d
Nazim Fataliev <nazim.rabota@bk.ru>
2025-07-27 16:20:06 +0300

    +

--- app/api/requests/route.ts
+++ app/api/requests/route.ts
@@ -1,13 +1,13 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
-
+import JSONBig from 'json-bigint';
 
 
 export  async function GET(req: NextRequest) {
       if (req.method === "GET") {
          try {
             const requests = await prisma.request.findMany();
-            return NextResponse.json(requests, { status: 200 });
+            return NextResponse.json(JSONBig.stringify(requests), { status: 200 });
          } catch (error) {
             console.log(error);
             return NextResponse.json({error: "Ошибка при получении постов"},{status: 500});
