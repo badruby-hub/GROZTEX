263fba3e9327ebe18f7e8ccde46c0567c579f53d
Nazim Fataliev <nazim.rabota@bk.ru>
2025-07-30 02:34:19 +0300

    исправление загрузки заявок

--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -1,7 +1,7 @@
 import classes from "./loading.module.css";
 
 export default function Loader(){
-    return  <div className={classes.body}> <div class={classes.L}>L</div>
+    return  <div className={classes.loader}> <div class={classes.L}>L</div>
   <div class={classes.outter}>
     <div class={classes.mid}>
       
