b9c9e5cce36fcc269bd217eb961e0062957c8883
Nazim Fataliev <nazim.rabota@bk.ru>
2025-07-30 03:06:43 +0300

    +

--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import classes from "./loading.module.css";
 
 export default function Loader(){
     return <div class={classes.circ}>
-  <div class={classes.load}>Loading . . . </div>
+  <div class={classes.load}>Загрузка . . . </div>
   <div class={classes.hands}></div>
   <div class={classes.body}></div>
   <div class={classes.head}>
