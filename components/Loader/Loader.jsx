5361244a162520ae550bd3a12af17c088b74ba62
Nazim Fataliev <nazim.rabota@bk.ru>
2025-07-30 03:59:11 +0300

    +

--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -1,12 +1,17 @@
 import classes from "./loading.module.css";
 
 export default function Loader(){
-    return <div class={classes.circ}>
-  <div class={classes.load}>Загрузка . . . </div>
-  <div class={classes.hands}></div>
-  <div class={classes.body}></div>
-  <div class={classes.head}>
-    <div class={classes.eye}></div>
-  </div>
-</div>
+    return      <div className={classes.loaderWrapper}>
+      <svg className={classes.spinner} viewBox="0 0 50 50" width="150" height="150" >
+        <circle
+          className={classes.path}
+          cx="25"
+          cy="25"
+          r="20"
+          fill="none"
+          strokeWidth="5"
+        />
+      </svg>
+      <div className={classes.text}>Загрузка</div>
+    </div>
 }
