631bc7abdd1694ade1daad0b936ead56db933c31
Nazim Fataliev <nazim.rabota@bk.ru>
2025-07-31 00:51:34 +0300

    страница администрации

--- app/api/user/admin/route.ts
+++ app/api/user/admin/route.ts
@@ -26,3 +26,35 @@ export async function GET(req: NextRequest) {
     await prisma.$disconnect();
   }
 }
+
+
+
+
+export async function PATCH(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const chatId = BigInt(body.chatId);
+
+    const user = await prisma.user.findUnique({
+      where: { chatId },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Пользователь не найден" }, { status: 404 });
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { chatId },
+      data: {
+        isAdmin: !user.isAdmin,
+      },
+    });
+
+    return NextResponse.json({ isAdmin: updatedUser.isAdmin });
+  } catch (error) {
+    console.error("Ошибка обновления прав:", error);
+    return NextResponse.json({ error: "Ошибка обновления прав" }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
--- components/Administrators/Administrators.jsx
+++ components/Administrators/Administrators.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react"
 import classes from "./administrators.module.css";
 import Loader from "../Loader/Loader";
+import toast from "react-hot-toast";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 
 export default function Administrators() {
  const [result, setResult] = useState([]);
@@ -15,6 +19,7 @@ export default function Administrators() {
        tg.BackButton.onClick(btnBackClick);
            async function users() {
         try {
+            
             setIsLoading(true);
             const response = await fetch(`/api/user`);
             const data = await response.json();
@@ -23,6 +28,7 @@ export default function Administrators() {
             console.error("Ошибка инициализации:", error);
         } finally{
             setIsLoading(false);
+            
         }
     }
     users()
@@ -31,34 +37,53 @@ export default function Administrators() {
       true: "Администратор",
       false: "Клиент"
    };
+     const flagSwitch = async (user)=>{
+        try {
+            NProgress.start();
+            await fetch("/api/user/admin",{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify({ chatId: user.chatId }),
+            });
+            const response = await fetch(`/api/user`);
+            const data = await response.json();
+            setResult(data);
+            toast.success(`${user.isAdmin?
+                 "Права администратора удалены"
+                  :
+                 "Права администратора выданы"}`);
+                 NProgress.done();
+        } catch (error) {
+                toast.error("Ошибка обновления прав");
+              console.error(error);
+        }
+     };
+
 
     return  <div className={classes.container__users}>
-         <h1 className={classes.zagolovok}>Пользователи</h1>
-         {isLoading? <Loader/> :
-          <table className={classes.block__info}>
-             <thead>
-                <tr>
-                 <th className={classes.th}>id</th>
-                 <th className={classes.th}>Администратор</th>
-                 <th className={classes.th}>ник тг</th>
-                 <th className={classes.th}>Имя</th>
-                 <th className={classes.th}>Фамилия</th>
-                 <th className={classes.th}>чат id</th>
-                </tr>
-            </thead>
-        <tbody>
-         {result.map((req)=>{
-            return<tr key={req.id}>
-                <td className={`${classes.id} ${classes.td}`}>{req.id || "Пусто"}</td>
-                <td className={`${classes.isAdmin} ${classes.td}`}>{status[req.isAdmin]}</td>
-                <td className={`${classes.userName} ${classes.td}`}>{req.userName || "Пусто"}</td>
-                <td className={`${classes.firstName} ${classes.td}`}>{req.firstName || "Пусто"}</td>
-                <td className={`${classes.lastName} ${classes.td}`}>{req.lastName || "Пусто"}</td>
-                <td className={`${classes.chatId} ${classes.td}`}>{req.chatId || "Пусто"}</td>
-                </tr>
-        })}
-        </tbody>
-      
-         </table>
+         <h1 className={classes.zagolovok}>Карточки пользователей</h1>
+         {isLoading? <Loader/> : result.map((user)=>{
+         return <div className={classes.card} key={user.id}>
+      <div className={classes.cardHeader}>
+        <span className={classes.role}>
+          {status[user.isAdmin]}
+        </span>
+        <span className={classes.userName}>
+          @{user.userName || "Пусто"}
+        </span>
+      </div>
+      <div className={classes.block__main}>
+      <div className={classes.cardBody}>
+        <div className={classes.id}><strong>ID:</strong> {user.id}</div>
+        <div className={classes.firstName}><strong>Имя:</strong> {user.firstName || "Пусто"}</div>
+        <div className={classes.lastName}><strong>Фамилия:</strong> {user.lastName || "Пусто"}</div>
+        <div className={classes.chatId}><strong>Chat ID:</strong> {user.chatId}</div>
+        </div>
+            <button onClick={()=>flagSwitch(user)} className={classes.btn__give}>{user.isAdmin ? "Удалить права": "Дать права"}</button>
+      </div>
+    </div>
+ })
 }</div>
 }
