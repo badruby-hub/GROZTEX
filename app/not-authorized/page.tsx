495d8b5592bd918fe19f6b80350398ffd4fcb429
Nazim Fataliev <nazim.rabota@bk.ru>
2025-08-01 16:11:24 +0300

    +

--- app/not-authorized/page.tsx
+++ app/not-authorized/page.tsx
@@ -1,6 +1,6 @@
 export default function NotAuthorizedPage() {
   return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
+    <div style={{ padding: "2rem", textAlign: "center", color: "#ffffff",position:"relative"}}>
       <h1>🚫 Доступ запрещён</h1>
       <p>У вас нет прав для доступа к этой странице.</p>
     </div>
