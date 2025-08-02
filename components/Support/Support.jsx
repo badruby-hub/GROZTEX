228df82e69e2f7ec68b61d2da1c5498c8257b675
Nazim Fataliev <nazim.rabota@bk.ru>
2025-08-02 22:31:43 +0300

    редакция

--- components/Support/Support.jsx
+++ components/Support/Support.jsx
@@ -29,7 +29,7 @@ useEffect(() => {
   <div className={classes.text__support}>
      <p> Наша операторы на связи 24/7. Готовы ответить на любые вопросы.</p>
      <p> Для покупки USDT, вам нужно создать заявку через бота</p>
-     <p> Для связи с нами напишите в Telegram канал —<Link  className={classes.link__tg} href="https://t.me/RoNin1411">GROZTEX__SUPPORT</Link></p>
+     <p> Для связи с нами напишите в Telegram канал —<Link  className={classes.link__tg} href="https://t.me/GROZTEX">GROZTEX__SUPPORT</Link></p>
   </div>
       <BtnChatTg/>
      <BtnBackHome/> 
