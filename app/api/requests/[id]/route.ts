17dd784c534692b7c01c9eace7cc80f02a38813d
Nazim Fataliev <nazim.rabota@bk.ru>
2025-08-05 01:54:00 +0300

    правки

--- app/api/requests/[id]/route.ts
+++ app/api/requests/[id]/route.ts
@@ -1,43 +1,43 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { number: string } }
+) {
+  try {
+    const { number } = params;
 
-export async function PATCH(req: NextRequest) {
-   try {
-    const url = new URL(req.url);
-    const pathnameParts = url.pathname.split("/"); 
-
-    const id = pathnameParts[pathnameParts.length - 1];
-
-    if (!id) {
+    if (!number) {
       return NextResponse.json({ error: "ID не передан" }, { status: 400 });
     }
-         const body = await req.json();
-         const status = body.status;
-         
-         if (!["PENDING", "ACCEPTED", "REJECTED"].includes(status)) {
+
+    const body = await req.json();
+    const status = body.status;
+
+    if (!["PENDING", "ACCEPTED", "REJECTED"].includes(status)) {
       return NextResponse.json({ error: "Некорректный статус" }, { status: 400 });
     }
 
-    const requestId = BigInt(id);
+    const requestId = BigInt(number);
 
-   const updatedRequest = await prisma.request.update({
+    const updatedRequest = await prisma.request.update({
       where: { number: requestId },
       data: { status },
     });
 
-
-  return NextResponse.json({
-      ...updatedRequest,
-      number: updatedRequest.number.toString(),
-      authorId: updatedRequest.authorId.toString(),
-    }, { status: 200 });
-
-
-   } catch (error) {
-       console.error(error);
+    return NextResponse.json(
+      {
+        ...updatedRequest,
+        number: updatedRequest.number.toString(),
+        authorId: updatedRequest.authorId.toString(),
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
     return NextResponse.json({ error: "Ошибка при обновлении заявки" }, { status: 500 });
-   } finally{
-      await prisma.$disconnect();
-   }
+  } finally {
+    await prisma.$disconnect();
+  }
 }
