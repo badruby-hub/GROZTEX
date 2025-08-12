1c0b889d2df3d6cfa4b2449933eb5b5177427454
Nazim Fataliev <nazim.rabota@bk.ru>
2025-08-12 17:12:17 +0300

    правки в блокировке

--- components/Users-control/blocked/Blocked-users.jsx
+++ components/Users-control/blocked/Blocked-users.jsx
@@ -1,8 +1,28 @@
 "use client";
+import BtnBackHome from "@/components/Button/BtnBackHome";
 import classes from "./Blocked.module.css";
+import { useEffect } from "react";
 
 export default function BlockedUsers() {
+    useEffect(() => {
+        const tg = window.Telegram.WebApp;
+           tg.BackButton.show();
+  
+      const btnBackClick = () => {
+        window.history.back();
+      };
+  
+      tg.BackButton.onClick(btnBackClick);
+
+      return () => {
+      tg.BackButton.hide();
+      tg.BackButton.offClick(btnBackClick);
+      };
+
+    
+       }, []);
   return <>
-    <h1 className={classes.zagolovok}>Блокировка</h1>
+    <h1 className={classes.zagolovok}>Заблокированные пользователи</h1>
+     <BtnBackHome/>
      </>    
 }
